e7320f96b4cd8009c1e724720f238686ff3c54b4
Mikhail <l_mikhail@inbox.ru>
2019-12-12 16:16:40 +0300

    js with comments

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function() { //waiting for DOM loaded - ждем, пока загрузится DOM дерево
     'use strict';
 
-    let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('.info-header'),
-        tabContent = document.querySelectorAll('.info-tabcontent');
-    function hideTabContent(a) {
+    let tab = document.querySelectorAll('.info-header-tab'), // receiving our TABS from html page - получаем наши табы (кнопки) с html странички по классу info-header-tab
+        info = document.querySelector('.info-header'), // receiving PARENT of our TABS - получаем родителя (обертку) наших табов по классу info-header
+        tabContent = document.querySelectorAll('.info-tabcontent'); // receiving content - получаем содержимое табов по классу info-tabcontent
+    function hideTabContent(a) {                                    // hiding all tabs contents exept first one - скрываем содержимое всех табов, кроме первого
         for (let i = a; i < tabContent.length; i++) {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
+            tabContent[i].classList.remove('show');                    // we use class SHOW in *.css -to show elements - используем класс show в файлике с css для эдементов ,которые хотим показать
+            tabContent[i].classList.add('hide');                       // we use class HIDE in *.css -to hide elements 
         }
     }
     hideTabContent(1);
